e4df8c82851a71df23007bdcb687e80409ab4b3f
AswinRamKumar <aswinramkumar5002@gmail.com>
2026-02-04 19:39:34 +0530

    Consolidate requirements and fix sqlite for cloud

--- github_rag_engine/app.py
+++ github_rag_engine/app.py
@@ -2,6 +2,13 @@ import streamlit as st
 import sys
 import os
 
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(ROOT_DIR)
 
